ec5b02db3476a78f481670271f5e0baec9c2b726
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-27 10:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+// Function to determine the DOM level of an element
+function getDomLevel(elementId) {
+    const targetElement = document.getElementById(elementId);
+
+    if (!targetElement) {
+        alert("Element not found.");
+        return;
+    }
+
+    let domLevel = 0;
+    let currentNode = targetElement;
+
+    while (currentNode !== document.documentElement) {
+        currentNode = currentNode.parentNode;
+        domLevel++;
+    }
+
+    // Add 1 to include the root html element
+    domLevel++;
+
+    alert(`The level of the element is: ${domLevel}`);
+}
+
+// Call the function with the element id "level"
+getDomLevel("level");
